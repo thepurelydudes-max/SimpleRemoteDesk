f021fdb2edaa7f4f7c3a40b41c0477e3aaed039f
thepurelydudes-max <thepurelydudes@gmail.com>
2026-09-26 00:47:58 +0300

    Make video client cancellable

--- native/src/video/video_channel.h
+++ native/src/video/video_channel.h
@@ -5,8 +5,14 @@
 #include <atomic>
 #include <cstdint>
 #include <functional>
+#include <memory>
+#include <mutex>
 #include <string>
 #include <thread>
+
+namespace srd::core {
+class Session;
+}
 
 namespace srd::video {
 
@@ -38,14 +44,23 @@ private:
 class VideoClient {
 public:
     VideoClient(std::string host, std::string password, std::uint16_t port = 45902);
+    ~VideoClient();
+
+    VideoClient(const VideoClient&) = delete;
+    VideoClient& operator=(const VideoClient&) = delete;
 
     void receive_forever(
         const std::function<void(EncodedFrame&&)>& onFrame);
+
+    void stop() noexcept;
 
 private:
     std::string host_;
     std::string password_;
     std::uint16_t port_;
+    std::atomic<bool> running_{false};
+    std::mutex sessionMutex_;
+    std::unique_ptr<core::Session> session_;
 };
 
 } // namespace srd::video

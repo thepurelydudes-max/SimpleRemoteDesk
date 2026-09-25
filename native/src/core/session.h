7959c86f3330bd8a0dad3b57bce88aa57cee1a81
thepurelydudes-max <thepurelydudes@gmail.com>
2026-09-26 00:22:55 +0300

    Implement M1 native session core

---
+++ native/src/core/session.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "core/tcp_socket.h"
+#include "protocol/message.h"
+
+#include <span>
+
+namespace srd::core {
+
+class Session {
+public:
+    explicit Session(net::TcpSocket socket);
+
+    void send(protocol::MessageType type, std::span<const std::byte> payload = {});
+    protocol::Message receive();
+
+private:
+    net::TcpSocket socket_;
+};
+
+} // namespace srd::core

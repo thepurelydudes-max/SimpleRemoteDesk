1ec3bf0104c6efb3e9d412c515376ab704e40b80
thepurelydudes-max <thepurelydudes@gmail.com>
2026-09-26 01:00:27 +0300

    Set discovery identity for console Host

--- native/src/host/main.cpp
+++ native/src/host/main.cpp
@@ -16,6 +16,7 @@ int main(int argc, char** argv)
     config.jpegQuality = argc > 4
         ? static_cast<float>(std::stoi(argv[4])) / 100.0f
         : 0.90f;
+    config.hostId = "console-host";
 
     try {
         srd::host::HostService host;

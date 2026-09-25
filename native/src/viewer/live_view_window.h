846c0b50a0055f88206f8e07747ada1a374d81b8
thepurelydudes-max <thepurelydudes@gmail.com>
2026-09-26 00:54:18 +0300

    Add remote input to live viewer window

--- native/src/viewer/live_view_window.h
+++ native/src/viewer/live_view_window.h
@@ -3,12 +3,19 @@
 #include "viewer/frame_mailbox.h"
 
 #include <windows.h>
+#include <cstdint>
+
+namespace srd::control {
+class ControlClient;
+}
 
 namespace srd::viewer {
 
 class LiveViewWindow {
 public:
-    explicit LiveViewWindow(DecodedFrameMailbox& mailbox);
+    LiveViewWindow(
+        DecodedFrameMailbox& mailbox,
+        control::ControlClient* control = nullptr);
 
     int run(HINSTANCE instance, int showCommand);
     void notify_frame() const noexcept;
@@ -29,9 +36,21 @@ private:
         LPARAM lParam);
 
     void paint(HWND hwnd);
+    bool map_to_remote(LPARAM lParam, std::int32_t& x, std::int32_t& y) const;
+    void toggle_fullscreen(HWND hwnd);
+    void release_pressed_keys() noexcept;
 
     DecodedFrameMailbox& mailbox_;
+    control::ControlClient* control_{nullptr};
     HWND hwnd_{nullptr};
+
+    bool fullscreen_{false};
+    RECT windowedRect_{};
+    DWORD windowedStyle_{0};
+    DWORD windowedExStyle_{0};
+
+    ULONGLONG lastMouseSend_{0};
+    bool pressedKeys_[256]{};
 };
 
 } // namespace srd::viewer
